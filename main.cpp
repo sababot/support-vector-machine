40fdc78210f71e1a6f70f6e42863dc2d205e47c0
sababot <twinsgardos@gmail.com>
2021-12-19 23:39:02 +0100

    research

--- main.cpp
+++ main.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
+void cost_function()
+{
+	
+}
+
 int main()
 {
+	// g(x') = w'T * x' + w0
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
+	// sample
 	// Code
 }
